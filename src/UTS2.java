f3934515568a11218cd635645737f38768112137
arisaefulhakim <118172320+arisaefulhakim@users.noreply.github.com>
2022-11-14 17:00:48 +0700

    Initial commit

---
+++ src/UTS2.java
@@ -0,0 +1,14 @@
+import java.security.PublicKey;
+
+public class UTS2 {
+    public static void angkarill() {
+        int[] angka = {2, 3, 5, 7, 11};
+        for (int index = 0; index < angka.length; index++) {
+            System.out.println("angka dalam aray= " + angka[index]);
+        }
+    }
+    public static void main(String[]args){
+        angkarill();
+
+    }
+}
